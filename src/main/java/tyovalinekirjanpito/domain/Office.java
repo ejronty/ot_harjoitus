986ebb69310beb11063acb53396b0c9d3980416d
Eelis <32396100+ejronty@users.noreply.github.com>
2020-12-10 19:21:19 +0200

    Toimipisteiden työvälineiden käsittelyä muutettu

--- src/main/java/tyovalinekirjanpito/domain/Office.java
+++ src/main/java/tyovalinekirjanpito/domain/Office.java
@@ -1,7 +1,8 @@
 
 package tyovalinekirjanpito.domain;
 
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * Toimipistettä kuvaava luokka.
@@ -9,54 +10,83 @@ import java.util.ArrayList;
 
 public class Office extends Thing {
 
-    private ArrayList<Tool> tools;
+    private TreeMap<String, Integer> tools;
 
     /**
      * Luo uuden toimipisteen. Metodia on tarkoitus käyttää, kun toimipiste
      * luodaan lukemalla tiedot tietokannasta, jolloin mukaan saadaan
      * myös toimipisteen id-tunnus.
+     * 
+     * @param name Luotavan toimipisteen nimi.
+     * @param id Toimipisteen id-tunnus.
      */
     public Office(String name, int id) {
         super(name, id);
-        this.tools = new ArrayList<>();
+        this.tools = new TreeMap<>();
     }
 
     public Office(String name) {
         super(name);
-        this.tools = new ArrayList<>();
+        this.tools = new TreeMap<>();
     }
 
-    public ArrayList<Tool> getTools() {
+    /**
+     * Metodi toimipisteen työvälineiden listaamiseen välittämättä määristä.
+     * 
+     * @return Toimipisteen työvälineiden nimet.
+     */
+    public Set<String> getToolNames() {
+        return this.tools.keySet();
+    }
+    
+    public TreeMap<String, Integer> getToolsWithAmounts() {
         return this.tools;
     }
-
-    public void setTools(ArrayList<Tool> tools) {
-        this.tools = tools;
+    
+    public int getAmount(String toolName) {
+        return this.tools.get(toolName);
+    }
+    
+    public boolean containsTool(String toolName) {
+        return this.tools.containsKey(toolName);
     }
 
     /**
      * Metodi työvälineen lisäämiseksi toimipisteelle. Tarkistaa, ettei
      * työväline ole jo ennestään listalla.
      * 
-     * @param tool Työväline, joka halutaan lisätä.
+     * @param toolName Lisättävän työvälineen nimi.
+     * @param amount Montako kappaletta välinettä lisätään.
      */
-    public boolean addTool(Tool tool) {
-        if (!(this.tools.contains(tool))) {
-            this.tools.add(tool);
+    public boolean addTool(String toolName, int amount) {
+        if (!(this.tools.containsKey(toolName))) {
+            this.tools.put(toolName, amount);
             return true;
         }
-
         return false;
+    }
+    
+    /**
+     * Päivittää työvälineen määrän tällä toimipisteellä.
+     * 
+     * @param toolName Päivitettävän työvälineen nimi.
+     * @param newAmount Työvälineen uusi määrä.
+     */
+    public boolean updateAmount(String toolName, int newAmount) {
+        if (this.tools.replace(toolName, newAmount) == null) {
+            return false;
+        }
+        return true;
     }
     
     /**
      * Metodi työvälineen poistamiseksi toimipisteestä.
      * 
-     * @param tool Työväline, joka halutaan poistaa.
+     * @param toolName Poistettavan työvälineen nimi.
      */
-    public boolean removeTool(Tool tool) {
-        if (this.tools.contains(tool)) {
-            this.tools.remove(tool);
+    public boolean removeTool(String toolName) {
+        if (this.tools.containsKey(toolName)) {
+            this.tools.remove(toolName);
             return true;
         }
         return false;
@@ -67,7 +97,6 @@ public class Office extends Thing {
         if (!(obj instanceof Office)) {
             return false;
         }
-        
         Office other = (Office) obj;
         return this.name.equalsIgnoreCase(other.getName());
     }
