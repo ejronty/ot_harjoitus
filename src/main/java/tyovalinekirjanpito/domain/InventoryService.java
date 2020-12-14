41c01f6d53331d30308a904ab55c68fdd34795dc
Eelis <32396100+ejronty@users.noreply.github.com>
2020-12-14 16:51:58 +0200

    Sovelluslogiikan refaktorointia

--- src/main/java/tyovalinekirjanpito/domain/InventoryService.java
+++ src/main/java/tyovalinekirjanpito/domain/InventoryService.java
@@ -23,64 +23,88 @@ public class InventoryService {
     }
 
     /**
-     * Uuden asian luomiseen käytettävä metodi.
+     * Luo uuden työvälineen.
      * 
-     * @param table tietokantataulu, johon tietoa halutaan lisätä.
-     * Tämä ei tule käyttäjän syötteestä, vaan käyttöliittymä huolehtii
-     * lähetettävän arvon valinnasta.
      * @param itemName Lisättävän kohteen nimi.
+     * @param consumable Onko kyseessä kuluva työväline?
      */
-    public boolean create(String table, String itemName) {
+    public boolean createTool(String itemName, boolean consumable) {
         String name = this.processInput(itemName);
 
         try {
-            if ("tools".equals(table)) {
-                this.toolDao.create(table, name);
-            } else if ("offices".equals(table)) {
-                this.officeDao.create(table, name);
+            this.toolDao.create(name, consumable);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Luo uuden toimipisteen.
+     * 
+     * @param officeName Luotavan toimipisteen nimi.
+     */
+    public boolean createOffice(String officeName) {
+        String name = this.processInput(officeName);
+
+        try {
+            this.officeDao.create(name);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Muokkaa työvälineen tietoja.
+     * 
+     * @param oldName Muokattavan työvälineen tämänhetkinen nimi.
+     * @param newName Työvälineen uusi nimi.
+     * @param consumable Onko työväline kuluva?
+     */
+    public boolean editTool(String oldName, String newName, boolean consumable) {
+        String name = this.processInput(newName);
+
+        try {
+            Tool tool = this.toolDao.findByName(oldName);
+            this.toolDao.edit(tool.getId(), name, consumable);
+            if (!oldName.equals(name)) {
+                this.updateToolInsideOffice(oldName, name);
             }
         } catch (Exception e) {
             return false;
         }
         return true;
     }
-    
+
+    private void updateToolInsideOffice(String oldName, String newName) throws Exception {
+        for (Office office : this.officeDao.getAll()) {
+            if (office.containsTool(oldName)) {
+                int amount = office.getAmount(oldName);
+                office.removeTool(oldName);
+                office.addTool(newName, amount);
+                this.officeDao.updateToolList(office);
+            }
+        }
+    }
+
     /**
-     * Kohteen uudelleennimeämiseen käytettävä metodi.
+     * Uudelleennimeää toimipisteen.
      * 
-     * @param table Uudelleennimettävän asian tyyppi, työväline vai 
-     * toimipiste. Ei käyttäjän syöte.
      * @param oldName Nimi, jolla muokattava kohde tällä hetkellä
      * esiintyy ohjelmassa.
      * @param newName Käyttäjän syöttämä uusi nimi kohteelle.
      */
-    public boolean rename(String table, String oldName, String newName) {
+    public boolean renameOffice(String oldName, String newName) {
         String name = this.processInput(newName);
 
         try {
-            if ("offices".equals(table)) {
-                Office office = this.officeDao.findByName(oldName);
-                this.officeDao.rename(table, office.getId(), name);
-            } else {
-                Tool tool = this.toolDao.findByName(oldName);
-                this.toolDao.rename(table, tool.getId(), name);
-
-                this.updateToolInsideOffice(tool, name);
-            }
-            return true;
+            Office office = this.officeDao.findByName(oldName);
+            this.officeDao.rename(office.getId(), name);
         } catch (Exception e) {
             return false;
         }
-    }
-    
-    private void updateToolInsideOffice(Tool tool, String newName) throws Exception {
-        for (Office office : this.officeDao.getAll()) {
-            if (office.getTools().contains(tool)) {
-                office.removeTool(tool);
-                office.addTool(new Tool(newName));
-                this.officeDao.updateToolList(office);
-            }
-        }
+        return true;
     }
 
     /**
@@ -98,18 +122,18 @@ public class InventoryService {
             } else {
                 Tool tool = this.toolDao.findByName(name);
                 this.toolDao.delete(table, tool.getId());
-                
-                this.deleteToolFromOffices(tool);
+
+                this.deleteToolFromOffices(name);
             }
             return true;
         } catch (Exception e) {
             return false;
         }
     }
-    
-    private void deleteToolFromOffices(Tool tool) throws Exception {
+
+    private void deleteToolFromOffices(String toolName) throws Exception {
         for (Office office : this.officeDao.getAll()) {
-            if (office.removeTool(tool)) {
+            if (office.removeTool(toolName)) {
                 this.officeDao.updateToolList(office);
             }
         }
@@ -121,14 +145,14 @@ public class InventoryService {
      * @param officeName Sen toimipisteen nimi, johon työväline halutaan
      * lisätä.
      * @param toolName Lisättävän työvälineen nimi.
+     * @param amount Montako kappaletta työvälinettä lisätään.
      */
-    public boolean addToolToOffice(String officeName, String toolName) {
+    public boolean addToolToOffice(String officeName, String toolName, int amount) {
 
         try {
             Office office = this.officeDao.findByName(officeName);
-            Tool tool = this.toolDao.findByName(toolName);
-            
-            if (office.addTool(tool)) {
+
+            if (office.addTool(toolName, amount)) {
                 this.officeDao.updateToolList(office);
             }
         } catch (Exception e) {
@@ -147,9 +171,8 @@ public class InventoryService {
     public boolean removeToolFromOffice(String officeName, String toolName) {
         try {
             Office office = this.officeDao.findByName(officeName);
-            Tool tool = this.toolDao.findByName(toolName);
 
-            if (office.removeTool(tool)) {
+            if (office.removeTool(toolName)) {
                 this.officeDao.updateToolList(office);
             }
         } catch (Exception e) {
@@ -189,10 +212,9 @@ public class InventoryService {
      */
     public Collection<String> findOfficesWithoutTool(String toolName) {
         try {
-            Tool tool = this.toolDao.findByName(toolName);
             return this.officeDao.getAll()
                     .stream()
-                    .filter(office -> !office.getTools().contains(tool))
+                    .filter(office -> !office.containsTool(toolName))
                     .map(office -> office.getName())
                     .collect(Collectors.toList());
         } catch (Exception e) {
@@ -207,10 +229,9 @@ public class InventoryService {
      */
     public Collection<String> findOfficesContainingTool(String toolName) {
         try {
-            Tool tool = this.toolDao.findByName(toolName);
             return this.officeDao.getAll()
                     .stream()
-                    .filter(office -> office.getTools().contains(tool))
+                    .filter(office -> office.containsTool(toolName))
                     .map(office -> office.getName())
                     .collect(Collectors.toList());
         } catch (Exception e) {
@@ -228,8 +249,8 @@ public class InventoryService {
             Office office = this.officeDao.findByName(officeName);
             return this.toolDao.getAll()
                     .stream()
-                    .filter(tool -> !office.getTools().contains(tool))
                     .map(tool -> tool.getName())
+                    .filter(toolName -> !office.containsTool(toolName))
                     .collect(Collectors.toList());
         } catch (Exception e) {
             return new ArrayList<>();
@@ -244,10 +265,7 @@ public class InventoryService {
     public Collection<String> findToolsInOffice(String officeName) {
         try {
             Office office = this.officeDao.findByName(officeName);
-            return office.getTools()
-                    .stream()
-                    .map(tool -> tool.getName())
-                    .collect(Collectors.toList());
+            return office.getToolNames();
         } catch (Exception e) {
             return new ArrayList<>();
         }
